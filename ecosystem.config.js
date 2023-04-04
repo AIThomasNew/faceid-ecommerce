5b24ffe2ccfd903bc9ca79f265b6fbc07c74b881
Yaroslav Kuznetsov <y.kuznetsov@admitad.com>
2023-04-05 00:57:34 +0600

    feat: add orm

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,13 @@ module.exports = {
       name: "backend",
       script: "./backend/server.js",
       exp_backoff_restart_delay: 100,
-      max_memory_restart: "300M",
+      max_memory_restart: "1048M",
     },
     {
       name: "client",
       script: "serve",
       exp_backoff_restart_delay: 100,
-      max_memory_restart: "300M",
+      max_memory_restart: "1048M",
       env: {
         PM2_SERVE_PATH: "./client/build/",
         PM2_SERVE_PORT: 3000,
